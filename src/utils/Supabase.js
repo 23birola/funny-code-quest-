920834603288a002b0dda1acc71c57f8fad4c911
23birola <72923832+23birola@users.noreply.github.com>
2024-03-08 23:34:59 +0000

    Update Supabase.js

--- src/utils/Supabase.js
+++ src/utils/Supabase.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 const supabaseUrl = 'https://ytipdxgirdozpzcdvtip.supabase.co';
 //const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0aXBkeGdpcmRvenB6Y2R2dGlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2OTY1NDksImV4cCI6MjAyNTI3MjU0OX0.nglExYZ7Tq9JtgaGM4pqvJC6-rNm0gfoTRsB_Zu4Nns';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, SUPABASE_API_KEY);
 
 // function that insert data to the supabase
 
